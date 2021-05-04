c39404eadfeb76af9107821e35d8b01de643c5e1
FlameWolf <ajifocus@LIVE.COM>
2021-05-04 12:43:16 +0530

    Removed the unnecessary `static` modifier from the CreateHostBuilder method.

--- AspDotNetCoreWithKestrelLesson/Program.cs
+++ AspDotNetCoreWithKestrelLesson/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-static IHostBuilder CreateHostBuilder(string[] args)
+IHostBuilder CreateHostBuilder(string[] args)
 {
 	return Host
 		.CreateDefaultBuilder(args)
