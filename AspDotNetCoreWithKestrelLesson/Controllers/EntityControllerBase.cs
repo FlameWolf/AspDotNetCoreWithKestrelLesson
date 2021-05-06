5c69b498ae45ba82b39f64fe34bc137a888948f0
FlameWolf <ajifocus@LIVE.COM>
2021-05-06 09:48:45 +0530

    Removed unnecessary `using` directives.

--- AspDotNetCoreWithKestrelLesson/Controllers/EntityControllerBase.cs
+++ AspDotNetCoreWithKestrelLesson/Controllers/EntityControllerBase.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
